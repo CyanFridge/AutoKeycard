8c76ebe5b541d33e32b5bc4563d932cd75727ff8
CyanFridge <thefridgeiscyan@gmail.com>
2026-08-05 02:57:20 -0700

    Proper Version Implenting

---
+++ 4.0.x/AutoKeycardConfig.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx.Configuration;
+
+namespace AutoKeycard
+{
+    public static class AutoKeycardConfig
+    {
+        public static ConfigEntry<bool> ShowRequiredKeycardName;
+        public static ConfigEntry<bool> ShowUsedKeycardMessage;
+        public static ConfigEntry<bool> EnableLogging;
+        public static ConfigEntry<bool> DebugLogging;
+
+        public static void Bind(ConfigFile config)
+        {
+            ShowRequiredKeycardName = config.Bind(
+                "General",
+                "Show Required Keycard Name",
+                true,
+                "Shows the required keycard name when missing one.");
+
+            ShowUsedKeycardMessage = config.Bind(
+                "General",
+                "Show Used Keycard Message",
+                true,
+                "Shows a notification when a keycard is used.");
+
+            EnableLogging = config.Bind(
+                "Logging",
+                "Enable Logging",
+                true,
+                "Enables normal AutoKeycard logging.");
+
+            DebugLogging = config.Bind(
+                "Logging",
+                "Debug Logging",
+                false,
+                "Enables extra debugging information.");
+        }
+    }
+}
