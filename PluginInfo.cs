1a44498934860800706963c0d5656e293bd0cdb3
CyanFridge <thefridgeiscyan@gmail.com>
2026-08-05 02:22:13 -0700

    Implementing Version Support

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoKeycard
-{
-    public static class PluginInfo
-    {
-        public const string GUID = "com.cyanfridge.autokeycard";
-        public const string NAME = "Auto Keycard";
-        public const string VERSION = "1.0.0";
-    }
-}
