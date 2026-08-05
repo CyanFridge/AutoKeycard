30e9d2ad9e2f00964fb1d3e70862ff61b4ec33ef
CyanFridge <thefridgeiscyan@gmail.com>
2026-08-05 05:12:51 -0700

    Fixes a few version issues.

--- 4.1.x/PluginInfo.cs
+++ 4.1.x/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.cyanfridge.autokeycard";
         public const string NAME = "Auto Keycard";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
     }
 }
